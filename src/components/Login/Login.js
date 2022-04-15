c911ee5f73fbe622fd92f641f84a7d69520a9aa3
Habibnusrat <habibucd@gmail.com>
2022-04-15 12:19:53 +0600

    addded on authstate change

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import useFirebase from '../../hooks/useFirebase';
 import './Login.css'
 
 const Login = () => {
+    const { singInWithGoogle } = useFirebase();
     return (
         <div>
-                <h3>Please Login</h3>
+            <h3>Please Login</h3>
+            <div style={{ margin: '20px' }}>
+                <button onClick={singInWithGoogle}>Google Sign In</button>
+            </div>
+            <br />
+
+            <form >
+                <input type="email" placeholder='Your Email' />
+                <br />
+                <input type="password" placeholder='Your Password' />
+                <br />
+                <input type="submit" value="Login" />
+            </form>
+
         </div>
     );
 };
---
+++ src/components/Register/Register.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Register.css'
+
+const Register = () => {
+    return (
+        <div>
+            <h3>Please Register now</h3>
+            <form >
+                <input type="text" placeholder='Your Name' />
+                <br />
+                <input type="email" placeholder='Your Email' />
+                <br />
+                <input type="password" placeholder='Your Password' />
+                <br />
+                <input type="submit" value="Register" />
+
+            </form>
+        </div>
+    );
+};
+
+export default Register;
